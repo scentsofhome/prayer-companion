4fcfa287451b2c0199611a591173fbba17e1f1ae
scentsofhome <scentsofhome4@gmail.com>
2026-07-01 14:30:07 -0400

    Cache Jordanville morning chunk 2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'orthodox-prayer-book-v19-jordanville-import';
+const CACHE_NAME = 'orthodox-prayer-book-v20-jordanville-import';
 const APP_SHELL = [
   './',
   './index.html',
@@ -10,6 +10,7 @@ const APP_SHELL = [
   './data/prayer-rules.json',
   './data/jordanville-import/manifest.json',
   './data/jordanville-import/morning-01.json',
+  './data/jordanville-import/morning-02.json',
   './data/psalm-50-51/01.txt',
   './data/psalm-50-51/02.txt',
   './data/psalm-50-51/03.txt',
